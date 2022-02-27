2818fb1004b3505723ecff130a8920256cba1dbf
Kinshuk <kinshuk.ram@gmail.com>
2022-02-27 21:45:16 +0100

    fix: Add spring docs to Balance app

--- balance-service/src/main/java/com/kadmos/services/balance/BalanceServiceApplication.java
+++ balance-service/src/main/java/com/kadmos/services/balance/BalanceServiceApplication.java
@@ -1,9 +1,14 @@
 package com.kadmos.services.balance;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info =
+@Info(title = "Balance API", version = "1.0", description = "Documentation for Balance API v1.0")
+)
 public class BalanceServiceApplication {
 
     public static void main(String[] args) {
